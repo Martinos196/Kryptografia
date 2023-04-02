6c0903e7ce2687fc9d410d1025ec8f70e4d7bb5b
Przemysław Musiał <126701320+PrzemyslawMusial242473@users.noreply.github.com>
2023-04-02 18:25:55 +0200

    Update AESTest.java

--- Zad1/src/test/java/kryptografia/AESTest.java
+++ Zad1/src/test/java/kryptografia/AESTest.java
@@ -1,101 +1,140 @@
 package kryptografia;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Random;
-import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
-public class ManualInputController implements Initializable {
+import static org.junit.jupiter.api.Assertions.*;
 
-    public TextField keyTextField;
-    public Button applyKeyButton;
-    public Button randomKeyButton;
-    public Button decryptButton;
-    public Button encryptButton;
-    public TextArea decipheredDataField;
-    public TextArea encryptedDataField;
-    public TextArea rawDataField;
-    public TextArea rawDataAsBytesArea;
-    public TextArea decipheredAsBytesField;
-    public MenuItem fileModeMenuItem;
-
+class AESTest {
     AES aes;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        keyTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
-            Pattern p = Pattern.compile("^[a-fA-F0-9]{32}$");
-            Matcher m = p.matcher(keyTextField.getText());
-            boolean b = m.matches();
-            applyKeyButton.setDisable(!b);
-        });
+    @BeforeEach
+    void init() throws Exception {
+        this.aes = new AES(new byte[]{63, -19, -50, -121, -122, 114, 53, 72, -87, -105, 72, 11, 11, 57, 52, -115});
     }
 
-    public void createAES(ActionEvent actionEvent) throws Exception {
-        byte[] key = aes.hexStringToByteArray(keyTextField.getText());
-        aes = new AES(key);
-        encryptButton.setDisable(false);
-        decryptButton.setDisable(false);
+    @Test
+    void shiftRowsTest() {
+        byte[] state = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        byte[] expected = new byte[]{0,5,10,15,4,9,14,3,8,13,2,7,12,1,6,11};
+        byte[] shifted = aes.shiftRows(state);
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
     }
 
-    public void generateRandomKey(ActionEvent actionEvent) {
-        String literals = "ABCDEF09876543210";
-        Random random = new Random();
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < 32; i++) {
-            builder.append(literals.charAt(random.nextInt(literals.length())));
-        }
-        keyTextField.textProperty().set(builder.toString());
+    @Test
+    void shiftArrayLeftTest() {
+        byte[] state = new byte[]{0,1,2,3};
+        byte[] shifted = aes.shiftArrayLeft(state,1);
+        byte[] expected = new byte[]{1,2,3,0};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
+
+        state = new byte[]{0,1,2,3};
+        shifted = aes.shiftArrayLeft(state,2);
+        expected = new byte[]{2,3,0,1};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
+
+        state = new byte[]{0,1,2,3};
+        shifted = aes.shiftArrayLeft(state,3);
+        expected = new byte[]{3,0,1,2};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
     }
 
-    public void encryptText(ActionEvent actionEvent) {
+    @Test
+    void shiftArrayRightTest() {
+        byte[] state = new byte[]{0,1,2,3};
+        byte[] shifted = aes.shiftArrayRight(state,1);
+        byte[] expected = new byte[]{3,0,1,2};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
 
-        rawDataAsBytesArea.setText(aes.bytesToHex(rawDataField.getText().getBytes(StandardCharsets.UTF_8)));
-        byte[] encoded = aes.encode(rawDataField.getText().getBytes(StandardCharsets.UTF_8));
+        state = new byte[]{0,1,2,3};
+        shifted = aes.shiftArrayRight(state,2);
+        expected = new byte[]{2,3,0,1};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
 
-        encryptedDataField.setText(aes.bytesToHex(encoded));
+        state = new byte[]{0,1,2,3};
+        shifted = aes.shiftArrayRight(state,3);
+        expected = new byte[]{1,2,3,0};
+        System.out.println(Arrays.toString(shifted));
+        assertArrayEquals(expected, shifted);
     }
 
+    @Test
+    void encryptTest() {
+        byte[] state = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        byte[] out = aes.encrypt(state);
+        System.out.println(Arrays.toString(state));
+        System.out.println(Arrays.toString(out));
+        assertNotNull(out);
 
-    public synchronized void decryptText(ActionEvent actionEvent) {
-        byte[] toDecrypt = aes.hexStringToByteArray(encryptedDataField.getText());
-        byte[] decrypted = aes.decode(toDecrypt);
-        String hex = aes.bytesToHex(decrypted);
-        decipheredAsBytesField.setText(hex);
-        byte[] bytes = new byte[0];
-        try {
-            bytes = Hex.decodeHex(hex.toCharArray());
-        } catch (DecoderException e) {
-            e.printStackTrace();
-        }
-        decipheredDataField.setText(new String(bytes, StandardCharsets.UTF_8));
+        byte[] state3 = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        byte[] out3 = aes.encrypt(state3);
+        System.out.println(Arrays.toString(state3));
+        System.out.println(Arrays.toString(out3));
+        assertNotNull(out3);
+
+        byte[] state2 = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16};
+        byte[] out2 = aes.encrypt(state2);
+        System.out.println(Arrays.toString(state2));
+        System.out.println(Arrays.toString(out2));
+        assertNotNull(out2);
+
     }
 
-    public void changeSceneToFileMode(ActionEvent actionEvent) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fileView.fxml"));
-            Scene scene = new Scene(root);
-            Stage window = (Stage) randomKeyButton.getScene().getWindow();
-            window.setScene(scene);
-            window.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Test
+    void decryptTest() {
+        byte[] state = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        byte[] out = aes.encrypt(state);
+        System.out.println(Arrays.toString(state));
+        System.out.println(Arrays.toString(out));
+        System.out.println("DECRYPTION:");
+        byte[] decrypted = aes.decrypt(out);
+        System.out.println(Arrays.toString(decrypted));
+        assertNotNull(out);
     }
+
+    @Test
+    void reverseKeyTest() {
+        System.out.println(Arrays.deepToString(aes.getKeyWords()));
+        System.out.println(Arrays.deepToString(aes.getKeyWordsReversed()));
+    }
+
+    @Test
+    void encodeDecodeTest() {
+        byte[] encoded = aes.encode("1234567891123456".getBytes());
+        byte[] encrypted = aes.encrypt("1234567891123456".getBytes());
+        assertArrayEquals(encoded, encrypted);
+
+        System.out.println(Arrays.toString("123".getBytes()));
+        encoded = aes.encode("123".getBytes());
+        System.out.println(Arrays.toString(encoded));
+        byte[] decoded = aes.decode(encoded);
+        System.out.println(Arrays.toString(decoded));
+    }
+    @Test
+    void polynomialMultiplicationTest() {
+        assertEquals(1, (aes.polynomialMultiplication((byte) 1, (byte) 1)));
+        assertEquals(0, (aes.polynomialMultiplication((byte) 0, (byte) 0)));
+        assertEquals(0b110011, aes.polynomialMultiplication((byte) 0b110011, (byte) 1));
+    }
+
+    @Test
+    void polynomialDivisionTest() {
+        assertEquals(0b1, aes.polynomialModuloDivision(0b1));
+        assertEquals(0b10, aes.polynomialModuloDivision(0b10));
+        assertEquals(0b100, aes.polynomialModuloDivision(0b100));
+    }
+
+    @Test
+    void convertStringToByteArrayTest() {
+        assertArrayEquals(new byte[]{1, 16}, aes.hexStringToByteArray("0110"));
+    }
+
 }
